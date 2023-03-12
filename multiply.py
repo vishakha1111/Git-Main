ef112779b055f7b3f4733a27911886a0605799ba
Vishakha Jadhav <jadhavvishakha1111@gmail.com>
2023-03-12 17:04:12 +0530

    added multiply.py file

---
+++ multiply.py
@@ -0,0 +1,9 @@
+num1 = 8
+num2 = 6.3
+
+# multiply two numbers
+diff = num1 * num2
+
+# Display the difference
+print('The mul of {0} and {1} is {2}'.format(num1, num2, diff))
+print('The result has been displayed.')
