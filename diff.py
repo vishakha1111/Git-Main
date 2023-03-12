3bc3db0f4d79c3f7a62af8652edc8168f300cef6
Vishakha Jadhav <jadhavvishakha1111@gmail.com>
2023-03-12 16:42:30 +0530

    addinf file diff.py

---
+++ diff.py
@@ -0,0 +1,10 @@
+
+num1 = 8
+num2 = 6.3
+
+# subtract two numbers
+diff = num1 - num2
+
+# Display the difference
+print('The diff of {0} and {1} is {2}'.format(num1, num2, diff))
+print('The result has been displayed.')
